bb13add54603dac8eb456a59fa910f30f4347bb6
SokolX <89soek@gmail.com>
2019-06-25 19:49:39 +0200

    feat: navigation with display flex

--- src/js/witaj.js
+++ src/js/witaj.js
@@ -2,3 +2,17 @@ const liczbaWejsc = 5;
 
 const about = document.querySelector('.about__paragraph--js'); 
 about.innerHTML = `Jestem tu po raz ${liczbaWejsc}`;
+
+const name = 'Łukasz';
+
+function witaj(name) {
+    alert(`Witam na stronie. ${name}`);
+}
+
+witaj(name);
+
+const witajConsolLog = (name) => {
+    console.log(`Witam na stronie. ${name}`);
+}
+
+witajConsolLog(name);
